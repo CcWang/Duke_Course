e131b78c91fe3e421b0090d9feedcdedfb30d4eb
Chongchong Wang <chongchong1110@gmail.com>
2016-10-17 00:02:59 -0700

    csvrecord

--- solutions/CSVProblem/WhichCountriesExport.java
+++ solutions/CSVProblem/WhichCountriesExport.java
@@ -41,20 +41,50 @@ public class WhichCountriesExport {
        
        }
    };
+   public static int numberOfExporters(CSVParser parser, String exportOfInetest){
+    int numbers=0;
+    for(CSVRecord record:parser){
+        String export = record.get("Exports");
+        if(export.contains(exportOfInetest)){
+            numbers++;
+        };
+    };
+    return numbers;
+   };
+   
+  public void bigExporters(CSVParser parser, String amount){
+    for(CSVRecord c:parser){
+        String values = c.get("Value (dollars)");
+        if(values.length()>amount.length()){
+                System.out.println(c.get("Country") + " "+ values);
+        };
+    };
+    };
+
    public void whoExportsCoffee(){
        FileResource fr= new FileResource();
        CSVParser parser = fr.getCSVParser();
-       listExporters(parser,"coffee");
+       listExporters(parser,"suger");
    };
    public void testCountryInfo(){
        FileResource fr= new FileResource();
        CSVParser parser = fr.getCSVParser();
        System.out.println(countryInfo(parser,"Germany"));
-       System.out.println(countryInfo(parser,"China"));
+       System.out.println(countryInfo(parser,"Nauru"));
    };
       public void testTwoProducts(){
        FileResource fr= new FileResource();
        CSVParser parser = fr.getCSVParser();
-       listExportersTwoProducts(parser,"gold","diamonds");
+       listExportersTwoProducts(parser,"fish","nuts");
+   };
+   public void testNumbers(){
+       FileResource fr= new FileResource();
+       CSVParser parser = fr.getCSVParser();
+       System.out.println(numberOfExporters(parser,"sugar"));
+   };
+   public void testBigExporters(){
+       FileResource fr= new FileResource();
+       CSVParser parser = fr.getCSVParser();
+       bigExporters(parser,"$999,999,999,999");
    };
 }
