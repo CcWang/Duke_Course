7c8dc98f34e660628710ce8a7d9e90c71597b732
Chongchong Wang <chongchong1110@gmail.com>
2016-10-30 00:11:47 -0700

    String

--- solutions/CaesarCipher/commonWords.java
+++ solutions/CaesarCipher/commonWords.java
@@ -1,4 +1,4 @@
-
+import edu.duke.*;
 /**
  * Write a description of commonWords here.
  * 
@@ -6,18 +6,46 @@
  * @version (a version number or a date)
  */
 public class commonWords {
-    
-    
+    //change common.txt words to an array
+    public String[] getCommon(){
+        FileResource resource = new FileResource("CommonWordsData/common.txt");
+        String[] common = new String[20];
+        int idx = 0;
+        for(String s:resource.words()){
+            common[idx]=s;
+            idx+=1;
+        }
+        return common;
+    };
+    public int indexOf(String[] common, String word){
+      for(int k=0;k<common.length;k++){
+        if(common[k].equals(word)){
+            return k;
+        }
+        
+        
+      };
+      return -1;
+    }
+    public void countWords(FileResource resource, String[] common, int[] counts){
+        for(String word:resource.words()){
+            word = word.toLowerCase();
+            int index = indexOf(common,word);
+            if(index !=-1){
+                counts[index] +=1;
+            };
+        }
+    };
     
     public void countShakespeare(){
         String[] plays={"caesar.txt","errors.txt","hamlet.txt","likeit.txt","macbeth.txt","romeo.txt"};
         
-        String common = getCommon();
+        String[] common = getCommon();
         int[] counts = new int[common.length];
         for(int i=0;i<plays.length;i++){
-            FileResource resource=new FileResource("CommonWordsData/"+plays[k]);
+            FileResource resource=new FileResource("CommonWordsData/"+plays[i]);
             countWords(resource, common,counts);
-            System.out.println("done with"+plays[k]);
+            System.out.println("done with"+plays[i]);
         };
         for (int k=0;k<common.length;k++){
             System.out.println(common[k]+"\t"+counts[k]);
